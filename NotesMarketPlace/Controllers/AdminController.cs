fd5b1461cb58910b956d4461d65c073453063f87
Yuvrajsinh-Zala <yuvrajsinhzala22210@gmail.com>
2021-03-04 09:21:15 +0530

    First commit

---
+++ NotesMarketPlace/Controllers/AdminController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NotesMarketPlace.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+        public ActionResult DownloadedNotes()
+        {
+            return View();
+        }
+        public ActionResult ManageAdministrator()
+        {
+            return View();
+        }
+
+        public ActionResult AddAdministrator()
+        {
+            return View();
+        }
+        public ActionResult ManageCategory()
+        {
+            return View();
+        }
+
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        public ActionResult ManageCountry()
+        {
+            return View();
+        }
+
+        public ActionResult AddCountry()
+        {
+            return View();
+        }
+
+        public ActionResult ManageSystemConfiguration()
+        {
+            return View();
+        }
+
+        public ActionResult ManageType()
+        {
+            return View();
+        }
+
+        public ActionResult AddType()
+        {
+            return View();
+        }
+
+        public ActionResult ManageMembers()
+        {
+            return View();
+        }
+
+        public ActionResult MyProfile()
+        {
+            return View();
+        }
+        public ActionResult NotesUnderPreview()
+        {
+            return View();
+        }
+
+        public ActionResult PublishedNotes()
+        {
+            return View();
+        }
+
+        public ActionResult RejectedNotes()
+        {
+            return View();
+        }
+
+        public ActionResult SpamReports()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ NotesMarketPlace/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NotesMarketPlace.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult BuyerRequests()
+        {
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult FAQ()
+        {
+            return View();
+        }
+
+        public ActionResult MyDownloadNotes()
+        {
+            return View();
+        }
+
+        public ActionResult MyProfile()
+        {
+            return View();
+        }
+
+        public ActionResult MyRejectedNotes()
+        {
+            return View();
+        }
+
+        public ActionResult MySoldNotes()
+        {
+            return View();
+        }
+
+        public ActionResult SearchNotes()
+        {
+            return View();
+        }
+
+    }
+}
