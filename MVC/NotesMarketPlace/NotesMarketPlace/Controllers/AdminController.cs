abaab57cb2c08289c3d6af3250c0cb27f099aa4c
Yuvrajsinh-Zala <yuvrajsinhzala22210@gmail.com>
2021-03-08 00:39:08 +0530

    MVC updated

---
+++ MVC/NotesMarketPlace/NotesMarketPlace/Controllers/AdminController.cs
@@ -0,0 +1,289 @@
+﻿using NotesMarketPlace.Database;
+using NotesMarketPlace.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NotesMarketPlace.Controllers
+{
+    [Authorize(Roles = "Admin, Super Admin")]
+    public class AdminController : Controller
+    {
+        private NotesMarketPlaceEntities _Context;
+        // GET: Admin
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+        public ActionResult DownloadedNotes()
+        {
+            return View();
+        }
+
+        public AdminController()
+        {
+            _Context = new NotesMarketPlaceEntities();
+        }
+
+        public ActionResult ManageAdministrator()
+        {
+            return View();
+        }
+
+        public ActionResult AddAdministrator()
+        {
+            return View();
+        }  
+        public ActionResult ManageCategory()
+        {
+            List<NoteCategory> NoteCategoriesList = _Context.NoteCategories.ToList(); //new List<NoteCategory>();
+            List<User> User = _Context.Users.ToList(); //new List<NoteCategory>();
+
+            var multiple = from c in NoteCategoriesList
+                           join t1 in User on c.CreatedBy equals t1.ID
+                           select new DataRetrival { NoteCategory = c, User = t1 };
+
+            return View(multiple);
+        }
+                
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+                
+        [HttpPost]
+        public ActionResult AddCategory(NoteCategory model)
+        {
+
+            bool isvalid = _Context.NoteCategories.Any(m => m.Name == model.Name);
+
+            if (!isvalid)
+            {
+                NoteCategory obj = new NoteCategory();
+                obj.Name = model.Name;
+                obj.Description = model.Description;
+                obj.CreatedDate = DateTime.Now;
+                obj.CreatedBy = 2;
+                obj.IsActive = true;
+
+                if (ModelState.IsValid)
+                {
+                    _Context.NoteCategories.Add(obj);
+                    try
+                    {
+                        // Your code...
+                        // Could also be before try if you know the exception occurs in SaveChanges
+
+                        _Context.SaveChanges();
+
+                        ModelState.Clear();
+
+                        return View();
+
+                    }
+                    catch (DbEntityValidationException e)
+                    {
+                        foreach (var eve in e.EntityValidationErrors)
+                        {
+                            Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                                eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                            foreach (var ve in eve.ValidationErrors)
+                            {
+                                Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                                    ve.PropertyName, ve.ErrorMessage);
+                            }
+                        }
+                    }
+
+                }
+
+            }
+
+            else
+                ViewBag.Message = "Note Category already exists";
+
+            return View();
+        }
+
+
+        public ActionResult ManageCountry()
+        {
+            List<Country> CountriesList = _Context.Countries.ToList(); //new List<Country>();
+            List<User> User = _Context.Users.ToList(); //new List<Country>();
+
+            var multiple2 = from c in CountriesList
+                            join t1 in User on c.CreatedBy equals t1.ID
+                            select new DataRetrival { Country = c, User = t1 };
+
+            return View(multiple2);
+        }
+
+        public ActionResult AddCountry()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCountry(Country model)
+        {
+
+            bool isvalid = _Context.Countries.Any(m => m.Name == model.Name);
+
+            if (!isvalid)
+            {
+                Country obj2 = new Country();
+                obj2.Name = model.Name;
+                obj2.CountryCode = model.CountryCode;
+                obj2.CreatedDate = DateTime.Now;
+                obj2.CreatedBy = 2;
+                obj2.IsActive = true;
+
+                if (ModelState.IsValid)
+                {
+                    _Context.Countries.Add(obj2);
+                    try
+                    {
+                        // Your code...
+                        // Could also be before try if you know the exception occurs in SaveChanges
+
+                        _Context.SaveChanges();
+
+                        ModelState.Clear();
+
+                        return View();
+
+                    }
+                    catch (DbEntityValidationException e)
+                    {
+                        foreach (var eve in e.EntityValidationErrors)
+                        {
+                            Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                                eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                            foreach (var ve in eve.ValidationErrors)
+                            {
+                                Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                                    ve.PropertyName, ve.ErrorMessage);
+                            }
+                        }
+                    }
+
+                }
+
+            }
+
+            else
+                ViewBag.Message = "Country already exists";
+
+            return View();
+        }
+        public ActionResult ManageSystemConfiguration()
+        {
+            return View();
+        }
+
+        public ActionResult ManageType()
+        {
+            List<NoteType> NoteTypesList = _Context.NoteTypes.ToList(); //new List<NoteType>();
+            List<User> User = _Context.Users.ToList(); //new List<NoteType>();
+
+            var multiple2 = from c in NoteTypesList
+                           join t1 in User on c.CreatedBy equals t1.ID
+                           select new DataRetrival { NoteType = c, User = t1 };
+
+            return View(multiple2);
+        }
+
+        public ActionResult AddType()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddType(NoteType model)
+        {
+
+            bool isvalid = _Context.NoteTypes.Any(m => m.Name == model.Name);
+
+            if (!isvalid)
+            {
+                NoteType obj3 = new NoteType();
+                obj3.Name = model.Name;
+                obj3.Description = model.Description;
+                obj3.CreatedDate = DateTime.Now;
+                obj3.CreatedBy = 2;
+                obj3.IsActive = true;
+
+                if (ModelState.IsValid)
+                {
+                    _Context.NoteTypes.Add(obj3);
+                    try
+                    {
+                        // Your code...
+                        // Could also be before try if you know the exception occurs in SaveChanges
+
+                        _Context.SaveChanges();
+
+                        ModelState.Clear();
+
+                        return View();
+
+                    }
+                    catch (DbEntityValidationException e)
+                    {
+                        foreach (var eve in e.EntityValidationErrors)
+                        {
+                            Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                                eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                            foreach (var ve in eve.ValidationErrors)
+                            {
+                                Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                                    ve.PropertyName, ve.ErrorMessage);
+                            }
+                        }
+                    }
+
+                }
+
+            }
+
+            else
+                ViewBag.Message = "Note Type already exists";
+
+            return View();
+        }
+
+        public ActionResult ManageMembers()
+        {
+            return View();
+        }
+
+        public ActionResult MyProfile()
+        {
+            return View();
+        }
+        public ActionResult NotesUnderPreview()
+        {
+            return View();
+        }
+
+        public ActionResult PublishedNotes()
+        {
+            return View();
+        }
+
+        public ActionResult RejectedNotes()
+        {
+            return View();
+        }
+
+        public ActionResult SpamReports()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MVC/NotesMarketPlace/NotesMarketPlace/Controllers/LoginController.cs
@@ -0,0 +1,227 @@
+﻿using NotesMarketPlace.Database;
+using NotesMarketPlace.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace NotesMarketPlace.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        private NotesMarketPlaceEntities _Context;
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public LoginController()
+            {
+                _Context = new NotesMarketPlaceEntities();
+            }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(login model)
+        {
+            bool isvalid = _Context.Users.Any(m => m.EmailID == model.EmailID && m.Password == model.Password);
+
+            if (isvalid)
+            {
+                var result = _Context.Users.Where(m => m.EmailID == model.EmailID).FirstOrDefault();
+                if (result.RoleID == 101 || result.RoleID == 102)
+                {
+                    FormsAuthentication.SetAuthCookie(model.EmailID, false);
+                    return RedirectToAction("Dashboard", "Admin");
+                }
+
+                else if (result.RoleID == 103)
+                {
+                    FormsAuthentication.SetAuthCookie(model.EmailID, false);
+                    return RedirectToAction("Dashboard", "User");
+                }
+                else
+                    ViewBag.NotValidUser = "Something went wrong";
+            }
+            else
+            {
+                ViewBag.NotValidUser = "Incorrect Email or Password";
+
+                /*if (model.Password == result.Password)
+                {
+                  /*  if (User.Identity.IsAuthenticated)
+                    {
+                        string name = User.Identity.Name;
+                    }
+                    if()
+                    FormsAuthentication.SetAuthCookie(result.EmailID,true);
+                    return RedirectToAction("", "user");
+                }
+                else
+                {
+                    ViewBag.NotValidPassword = "Passowrd is Incorrect";
+                }*/
+            }
+            return View("Login");
+
+        }
+    
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        NotesMarketPlaceEntities dbobj = new NotesMarketPlaceEntities();
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(User model)
+        {
+            var context = new NotesMarketPlaceEntities();
+            string email = model.EmailID;
+
+            if (IsValidEmail(email))
+            {
+                if (IsValidPassword(model.Password, model.RePassword))
+                {
+                    var result = context.Users.Where(m => m.EmailID == email).FirstOrDefault();
+                    if (result == null)
+                    {
+
+                        User obj = new User();
+               
+                        obj.FirstName = model.FirstName;
+                        obj.LastName = model.LastName;
+                        obj.EmailID = model.EmailID;
+                        obj.Password = model.Password;
+                        obj.RePassword = "abbc";
+                        obj.IsEmailVerified = false;
+                        obj.IsActive = true;
+                        obj.ModifiedBy = null;
+                        obj.ModifiedDate = null;
+
+                        obj.CreatedDate = DateTime.Now;
+                        obj.CreatedBy = null;
+                        obj.RoleID = 103;
+
+                        if (ModelState.IsValid)
+                        {
+                            dbobj.Users.Add(obj);
+                            try
+                            {
+                                // Your code...
+                                // Could also be before try if you know the exception occurs in SaveChanges
+
+                                dbobj.SaveChanges();
+                                ModelState.Clear();
+
+                                FormsAuthentication.SetAuthCookie(model.EmailID, true);
+                                return RedirectToAction("MyProfile", "User");
+
+                            }
+                            catch (DbEntityValidationException e)
+                            {
+                                foreach (var eve in e.EntityValidationErrors)
+                                {
+                                    Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                                        eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                                    foreach (var ve in eve.ValidationErrors)
+                                    {
+                                        Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                                            ve.PropertyName, ve.ErrorMessage);
+                                    }
+                                }
+                            }
+
+                        }
+
+                        return RedirectToAction("Home", "Home");
+
+                    }
+
+                    else
+                    {
+                        ViewBag.UserExist = "This Email is already exists";
+
+                    }
+
+
+
+                }
+
+                else
+                {
+                    ViewBag.NotValidPassword = "Password and Re-enter password must be same";
+                }
+            }
+            else
+            {
+                ViewBag.NotValidEmail = "Email is not valid";
+            }
+            return View("Signup");
+
+        }
+        public static bool IsValidPassword(string pswd, string repswd)
+        {
+            if (pswd == repswd && pswd != "")
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            try
+            {
+                // Normalize the domain
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                // Examines the domain part of the email and normalizes it.
+                string DomainMapper(Match match)
+                {
+                    // Use IdnMapping class to convert Unicode domain names.
+                    var idn = new IdnMapping();
+
+                    // Pull out and process domain name (throws ArgumentException on invalid)
+                    string domainName = idn.GetAscii(match.Groups[2].Value);
+
+                    return match.Groups[1].Value + domainName;
+                }
+            }
+            catch (RegexMatchTimeoutException e)
+            {
+                return false;
+            }
+            catch (ArgumentException e)
+            {
+                return false;
+            }
+
+            try
+            {
+                return Regex.IsMatch(email,
+                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+    }
+}
