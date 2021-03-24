e9c96cf9d1ebd69dd11702ae74aa9ad00f79fa67
Yuvrajsinh-Zala <yuvrajsinhzala22210@gmail.com>
2021-03-24 17:19:58 +0530

    MVC updated

--- MVC/NotesMarketPlace/NotesMarketPlace/Database/Model1.Designer.cs
+++ MVC/NotesMarketPlace/NotesMarketPlace/Database/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\VS\1\NotesMarketPlace\NotesMarketPlace\Database\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\VS\NotesMarketPlace\NotesMarketPlace\MVC\NotesMarketPlace\NotesMarketPlace\Database\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
