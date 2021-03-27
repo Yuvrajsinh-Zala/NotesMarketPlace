77c67ae7e6bce1f74cccd69b9d4b74c3236df4b0
Yuvrajsinh-Zala <yuvrajsinhzala22210@gmail.com>
2021-03-28 00:00:44 +0530

    MVC updated 2

--- MVC/NotesMarketPlace/NotesMarketPlace/Controllers/UserController.cs
+++ MVC/NotesMarketPlace/NotesMarketPlace/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using NoteMarketPlace.Models;
 using NotesMarketPlace.Database;
+using NotesMarketPlace.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -73,29 +74,228 @@ namespace NotesMarketPlace.Controllers
             return View();
         }
 
-        public ActionResult MyDownloadNotes()
-        {
-            return View();
-        }
-
         public ActionResult MyProfile()
-        {
-            return View();
-        }
-
-        public ActionResult MyRejectedNotes()
         {
             return View();
         }
 
         public ActionResult MySoldNotes()
         {
-            return View();
+            ViewBag.Count = 1;
+
+            List<User> tblUsersList = dbobj.Users.ToList();
+            List<Download> tblDownloadList = dbobj.Downloads.ToList();
+            List<UserProfile> tblUserProfilesList = dbobj.UserProfiles.ToList();
+
+            int user_id = (from user in dbobj.Users where user.EmailID == User.Identity.Name select user.ID).FirstOrDefault();
+
+            var multiple = (from d in tblDownloadList
+                            join t1 in tblUsersList on d.Downloader equals t1.ID
+                            join t2 in tblUserProfilesList on d.Downloader equals t2.UserID
+                            where d.Seller == user_id && d.IsSellerHasAllowedDownload == true
+
+
+                            select new DataRetrival { Download = d, User = t1, UserProfile = t2 }).ToList();
+
+            return View(multiple);
+
+        }
+
+
+        public ActionResult MyDownloadNotes(int id)
+        {
+
+            var user_id = dbobj.Users.Where(m => m.EmailID.Equals(User.Identity.Name)).FirstOrDefault();
+            var download = dbobj.Downloads.Where(m => m.NoteID.Equals(id) && m.Seller == user_id.ID).FirstOrDefault();
+
+            var attachments = dbobj.SellerNotesAttachements.Where(m => m.NoteID == id).FirstOrDefault();
+            var seller = dbobj.SellerNotes.Where(m => m.ID == id && m.SellerID == user_id.ID && m.Status == 10).FirstOrDefault();
+            if (seller != null || download != null)
+            {
+                string path = attachments.FilePath;
+                string filename = attachments.FileName + ".pdf";
+                byte[] fileBytes = System.IO.File.ReadAllBytes(path);
+
+                return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, filename);
+            }
+            else
+                return HttpNotFound();
+        }
+
+        public ActionResult MyRejectedNotes()
+        {
+
+            List<User> tblUsersList = dbobj.Users.ToList();
+            List<SellerNote> tblSellerNotes = dbobj.SellerNotes.ToList();
+            List<NoteCategory> tblNoteCategories = dbobj.NoteCategories.ToList();
+
+            int user_id = (from user in dbobj.Users where user.EmailID == User.Identity.Name select user.ID).FirstOrDefault();
+
+            ViewBag.Count = 1;
+            var multiple = (from d in tblSellerNotes
+                            join t1 in tblUsersList on d.SellerID equals t1.ID
+                            join t2 in tblNoteCategories on d.Category equals t2.ID
+                            where d.SellerID == user_id && d.Status == 10
+
+
+                            select new DataRetrival { SellerNote = d, User = t1, NoteCategory = t2 }).ToList();
+
+            return View(multiple);
         }
 
         public ActionResult SearchNotes()
         {
-            return View();
+            ViewBag.Message = "Search Notes page.";
+
+            List<SellerNote> SellerNotes = dbobj.SellerNotes.ToList();
+            List<Country> Countries = dbobj.Countries.ToList();
+
+            var multiple = from c in SellerNotes
+                           join t1 in Countries on c.Country equals t1.ID
+                           where c.Status == 9
+                           select new DataRetrival { SellerNote = c, Country = t1 };
+
+            ViewBag.Count = (from c in SellerNotes
+                             join t1 in Countries on c.Country equals t1.ID
+                             where c.Status == 9
+                             select c).Count();
+
+            return View(multiple);
+        }
+
+        public ActionResult NoteDeatils(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //tblSellerNote tblSeller = dbobj.tblSellerNotes.Find(id).;
+            var user_id = dbobj.Users.Where(m => m.EmailID == User.Identity.Name && m.RoleID != 103).FirstOrDefault();
+            if (user_id != null)
+                goto eligible;
+            var tblSeller = dbobj.SellerNotes.Where(m => m.ID == id && m.Status == 9).FirstOrDefault();
+            if (tblSeller == null)
+                return HttpNotFound();
+            eligible:
+            List<SellerNote> SellerNotes = dbobj.SellerNotes.ToList();
+            List<Country> Countries = dbobj.Countries.ToList();
+            List<NoteCategory> NoteCategories = dbobj.NoteCategories.ToList();
+
+            var multiple = from c in SellerNotes
+                           join t1 in Countries on c.Country equals t1.ID
+                           join t2 in NoteCategories on c.Category equals t2.ID
+                           where c.ID == id
+                           select new DataRetrival { SellerNote = c, Country = t1, NoteCategory = t2 };
+
+            return View(multiple);
+        }
+
+
+        public ActionResult FreeDownLoad(int? id)
+        {
+
+
+            var user_email = dbobj.Users.Where(m => m.EmailID.Equals(User.Identity.Name)).FirstOrDefault();
+
+            var tblSeller = dbobj.SellerNotes.Where(m => m.ID == id).FirstOrDefault();
+            var user_id = user_email.ID;
+
+            /* if (id == null)
+             {
+                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+             }
+             else*/
+            if (!tblSeller.IsPaid)
+            {
+                if (tblSeller == null || tblSeller.Status != 9)
+                    return HttpNotFound();
+
+                else if (tblSeller != null && tblSeller.Status == 9)
+                {
+
+                    string path = (from sa in dbobj.SellerNotesAttachements where sa.NoteID == tblSeller.ID select sa.FilePath).First().ToString();
+                    string category = (from c in dbobj.NoteCategories where c.ID == tblSeller.Category select c.Name).First().ToString();
+                    Download obj = new Download();
+                    obj.NoteID = tblSeller.ID;
+                    obj.Seller = tblSeller.SellerID;
+                    obj.Downloader = user_id;
+                    obj.IsSellerHasAllowedDownload = true;
+                    obj.AttachmentPath = path;
+                    obj.IsAttachmentDownloaded = true;
+                    obj.IsPaid = false;
+                    obj.PurchasedPrice = tblSeller.SellingPrice;
+                    obj.NoteTitle = tblSeller.Title;
+                    obj.NoteCategory = category;
+
+                    obj.CreatedDate = DateTime.Now;
+                    dbobj.Downloads.Add(obj);
+                    dbobj.SaveChanges();
+
+                    string filename = (from sa in dbobj.SellerNotesAttachements where sa.NoteID == id select sa.FileName).First().ToString();
+                    filename += ".pdf";
+                    byte[] fileBytes = System.IO.File.ReadAllBytes(path);
+
+                    return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, filename);
+
+                }
+            }
+            return HttpNotFound();
+
+        }
+
+
+        public ActionResult AskforDownload(int id)
+        {
+            var user_email = dbobj.Users.Where(m => m.EmailID.Equals(User.Identity.Name)).FirstOrDefault();
+
+            var tblSeller = dbobj.SellerNotes.Where(m => m.ID == id).FirstOrDefault();
+            var user_id = user_email.ID;
+            var check_download = dbobj.Downloads.Where(m => m.NoteID == id && m.Downloader == user_id).FirstOrDefault();
+
+            if (check_download != null)
+                return Json(new { success = true, alertMsg = "you downloaded this book already." }, JsonRequestBehavior.AllowGet);
+
+
+
+            //tblSellerNote tblSeller = dbobj.tblSellerNotes.Find(id).;
+            if (tblSeller == null || tblSeller.Status != 9)
+                return HttpNotFound();
+
+            else if (tblSeller != null && tblSeller.Status == 9)
+            {
+
+
+                var seller = dbobj.Users.Where(m => m.ID == tblSeller.SellerID).FirstOrDefault();
+                string path = (from sa in dbobj.SellerNotesAttachements where sa.NoteID == tblSeller.ID select sa.FilePath).First().ToString();
+                string category = (from c in dbobj.NoteCategories where c.ID == tblSeller.Category select c.Name).First().ToString();
+                string seller_name = seller.FirstName;
+                seller_name += " " + seller.LastName;
+                string buyer_name = user_email.FirstName;
+                buyer_name += " " + user_email.LastName;
+                string buyer_email = seller.EmailID;
+
+                Download obj = new Download();
+                obj.NoteID = tblSeller.ID;
+                obj.Seller = tblSeller.SellerID;
+                obj.Downloader = user_id;
+                obj.IsSellerHasAllowedDownload = false;
+                obj.AttachmentPath = path;
+                obj.IsAttachmentDownloaded = false;
+                obj.IsPaid = true;
+                obj.PurchasedPrice = tblSeller.SellingPrice;
+                obj.NoteTitle = tblSeller.Title;
+                obj.NoteCategory = category;
+                obj.CreatedDate = DateTime.Now;
+
+                dbobj.Downloads.Add(obj);
+                dbobj.SaveChanges();
+                ViewBag.Msg = "Request Added";
+
+                return Json(new { success = true, responseText = seller_name }, JsonRequestBehavior.AllowGet);
+
+            }
+
+            return Json(new { success = false, responseText = "Not Completed." }, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult AddNotes()
@@ -105,21 +305,15 @@ namespace NotesMarketPlace.Controllers
             SelectList list = new SelectList(NoteCategory, "ID", "Name");
             ViewBag.NoteCategory = list;
 
-
             var NoteType = entities.NoteTypes.ToList();
             SelectList NoteTypelist = new SelectList(NoteType, "ID", "Name");
             ViewBag.NoteType = NoteTypelist;
-
 
             var CountryName = entities.Countries.ToList();
             SelectList CountryList = new SelectList(CountryName, "ID", "Name");
             ViewBag.Country = CountryList;
 
-
-
             return View();
-
-
 
         }
 
@@ -128,27 +322,18 @@ namespace NotesMarketPlace.Controllers
         public ActionResult AddNotes(SellerNote model)
         {
 
-
             NotesMarketPlaceEntities entities = new NotesMarketPlaceEntities();
             var NoteCategory = entities.NoteCategories.ToList();
             SelectList list = new SelectList(NoteCategory, "ID", "Name");
             ViewBag.NoteCategory = list;
 
-
             var NoteType = entities.NoteTypes.ToList();
             SelectList NoteTypelist = new SelectList(NoteType, "ID", "Name");
             ViewBag.NoteType = NoteTypelist;
 
-
             var CountryName = entities.Countries.ToList();
             SelectList CountryList = new SelectList(CountryName, "ID", "Name");
             ViewBag.Country = CountryList;
-
-
-
-
-
-
 
             string name = User.Identity.Name;
             int u = (from user in dbobj.Users where user.EmailID == name select user.ID).Single();
@@ -160,18 +345,12 @@ namespace NotesMarketPlace.Controllers
             SellerNote obj = new SellerNote();
 
 
-
-
-
             if (model.uploadNote != null)
             {
                 string notepath = Server.MapPath("~/App_Data/Archive");
                 string notename = Path.GetFileName(model.uploadNote.FileName);
                 notename_fullpath = Path.Combine(notepath, notename);
                 model.uploadNote.SaveAs(notename_fullpath);
-
-
-
 
             }
             else
@@ -188,7 +367,6 @@ namespace NotesMarketPlace.Controllers
                 model.displayPic.SaveAs(picname_fullpath);
                 obj.DisplayPicture = picname_fullpath;
 
-
             }
             if (model.notePreview != null)
             {
@@ -201,7 +379,6 @@ namespace NotesMarketPlace.Controllers
 
             try
             {
-
 
                 obj.SellerID = u;
                 obj.Title = model.Title;
@@ -222,7 +399,6 @@ namespace NotesMarketPlace.Controllers
 
                 dbobj.SellerNotes.Add(obj);
                 dbobj.SaveChanges();
-
 
             }
             catch (System.Data.Entity.Validation.DbEntityValidationException dbEx)
--- MVC/NotesMarketPlace/NotesMarketPlace/Models/DataRetrival.cs
+++ MVC/NotesMarketPlace/NotesMarketPlace/Models/DataRetrival.cs
@@ -8,10 +8,11 @@ namespace NotesMarketPlace.Models
     public class DataRetrival
     {
         public Database.NoteCategory NoteCategory { get; set; }
+        public Database.SellerNote SellerNote { get; set; }
         public Database.User User { get; set; }
-
         public Database.NoteType NoteType { get; set; }
-
+        public Database.Download Download { get; set; }
+        public Database.UserProfile UserProfile { get; set; }
         public Database.Country Country { get; set; }
     }
 }
